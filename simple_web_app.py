41d065b1825cf4abc503fa605143e66ac1843f59
ovilogic <ovidiu.opreah@yahoo.com>
2024-03-25 21:27:10 +0000

    the most basic web app using flask. First time I'm using flask

---
+++ simple_web_app.py
@@ -0,0 +1,14 @@
+#! python
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
