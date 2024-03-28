5902d3ae34b3022665fcb40021643a9489531dc8
ovilogic <ovidiu.opreah@yahoo.com>
2024-03-28 20:03:48 +0000

    final form of the web app. But uploading it to the instance did not work so I had to create a venv from scratch on the instance. To adapt to the flask app, I had to create a rule for my security group, so that it would allow inbound http requests on port 8080, same as the flask app. This was done with Custom TCP rule, which allows you to specify the port.

--- simple_web_app.py
+++ simple_web_app.py
@@ -6,9 +6,17 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+@app.route('/fibonacci/<int:n>')
+def fibo(n):
+    return f"The {n}th Fibonacci number is {fibonacci(n)}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8080)
